62f9a21e4e9e6a875317596e15d93a57c78013d7
Aaron <aaronthesnowboarder@live.com>
2021-03-05 15:07:58 -0800

    started working on the logic on timeboxes. making them grey when the hour has past

--- DayPlanner/Develop/script.js
+++ DayPlanner/Develop/script.js
@@ -1,15 +1,32 @@
 let date =moment();
 $('#date').text(date.format("MMM Do, YYYY"));
 
-let nonStringHour =moment();
-$('').text(nonStringHour.format("LT"));
-let strignHour=moment(nonStringHour).format("h")
-console.log(strignHour)
-let hour=JSON.stringify(strignHour)
+let nonFormatHour =moment();
+$('').text(nonFormatHour.format("LT"));
+let hour=moment(nonFormatHour).format("H")
 console.log(hour)
 
 // if(strignHour==="2"){
 //     document.getElementById('twoBox').style.backgroundColor="red"
 //     console.log("test")
 // }
+
+if(hour>9){
+document.getElementById("nineBox").style.backgroundColor="grey"
+}
+if (hour===9){
+    document.getElementById("nineBox").style.backgroundColor="red"
+}
+if(hour )
+
+
+
+
+
+
+
+
+
+
+
 
