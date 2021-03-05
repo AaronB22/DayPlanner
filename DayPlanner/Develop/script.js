cec05a63e7007bb59dc60c8b5816358ae74d88d4
Aaron <aaronthesnowboarder@live.com>
2021-03-05 14:23:37 -0800

    more work on styling of the textboxes

--- DayPlanner/Develop/script.js
+++ DayPlanner/Develop/script.js
@@ -1,3 +1,15 @@
 let date =moment();
-
 $('#date').text(date.format("MMM Do, YYYY"));
+
+let nonStringHour =moment();
+$('').text(nonStringHour.format("LT"));
+let strignHour=moment(nonStringHour).format("h")
+console.log(strignHour)
+let hour=JSON.stringify(strignHour)
+console.log(hour)
+
+// if(strignHour==="2"){
+//     document.getElementById('twoBox').style.backgroundColor="red"
+//     console.log("test")
+// }
+
