7d83b3340abe08814a29a3f88b11bd8ad3255ba6
Aaron <aaronthesnowboarder@live.com>
2021-03-05 20:48:31 -0800

    logic for textbox9am is finished

--- DayPlanner/Develop/script.js
+++ DayPlanner/Develop/script.js
@@ -1,73 +1,97 @@
 let date =moment();
 $('#date').text(date.format("MMM Do, YYYY"));
 
-let nonFormatHour =moment();
-$('').text(nonFormatHour.format("LT"));
-let hour1=moment(nonFormatHour).format("H")
-let hour =Math.floor(hour1)
-
-//  let hour=15
+// let nonFormatHour =moment();
+// $('').text(nonFormatHour.format("LT"));
+// let hour1=moment(nonFormatHour).format("H")
+// let hour =Math.floor(hour1)
+ let hour=18
 //deubber tool. Too use commet out JS4-7 and unncommet out js9 
+
+const buttonPress = document.querySelector(".sb")
+buttonPress.addEventListener("click", funBtn)
+const savedButton = [b0,b1,b2,b3,b4,b5,b6,b7]
+const inputArray= [nineBox,tenBox,elevenBox,twelveBox,oneBox,twoBox,threeBox,fourBox,fiveBox]
+
+
 console.log(hour)
-     
-
-
 if(hour>9){
-document.getElementById("nineBox").style.backgroundColor="grey"
+inputArray[0].style.backgroundColor="grey"
 }
 if (hour===9){
-    document.getElementById("nineBox").style.backgroundColor="red"
+    inputArray[0].style.backgroundColor="red"
 }
 if(hour>10){
-    document.getElementById("tenBox").style.backgroundColor="grey"
+    inputArray[1].style.backgroundColor="grey"
 }
 if (hour===10){
-    document.getElementById("tenBox").style.backgroundColor="red"
+    inputArray[1].style.backgroundColor="red"
 }
 if (hour>11){
-    document.getElementById("elevenBox").style.backgroundColor="grey"
+    inputArray[2].style.backgroundColor="grey"
 }
 if(hour===11){
-    document.getElementById("elevenBox").style.backgroundColor="red"
+    inputArray[2].style.backgroundColor="red"
 }
 if(hour>12){
-    document.getElementById("twelveBox").style.backgroundColor="grey"
+    inputArray[3].style.backgroundColor="grey"
 }
 if(hour===12){
-document.getElementById("twelveBox").style.backgroundColor="red"
+inputArray[3].style.backgroundColor="red"
 }
 if (hour===13){
-    document.getElementById("oneBox").style.backgroundColor="red"
+    inputArray[4].style.backgroundColor="red"
 }
 if(hour>13){
-    document.getElementById("oneBox").style.backgroundColor="grey"
+    inputArray[4].style.backgroundColor="grey"
 }
 if (hour===14){
-    document.getElementById("twoBox").style.backgroundColor="red"
+    inputArray[5].style.backgroundColor="red"
 }
 if (hour>14){
-    document.getElementById("twoBox").style.backgroundColor="grey"
+    inputArray[5].style.backgroundColor="grey"
 }
 if(hour===15){
-    document.getElementById("threeBox").style.backgroundColor="red"
+    inputArray[6].style.backgroundColor="red"
 }
 if(hour>15){
-    document.getElementById("threeBox").style.backgroundColor="grey"
+    inputArray[6].style.backgroundColor="grey"
 }
-
 if(hour===16){
-    document.getElementById("fourBox").style.backgroundColor="red"
+    inputArray[7].style.backgroundColor="red"
 }
 if(hour>16){
-    document.getElementById("fourBox").style.backgroundColor="grey"
+    inputArray[7].style.backgroundColor="grey"
 }
-
 if(hour===17){
-    document.getElementById("fiveBox").style.backgroundColor="red"
+    inputArray[8].style.backgroundColor="red"
 }
 if(hour>17){
-    document.getElementById("fiveBox").style.backgroundColor="grey"
+    inputArray[8].style.backgroundColor="grey"
 }
+
+//local storage
+inputArray[0].value=localStorage.getItem("storage9")
+inputArray[1].value=localStorage.getItem("storage10")
+inputArray[2].value=localStorage.getItem("storage11")
+inputArray[3].value=localStorage.getItem("storage12")
+inputArray[4].value=localStorage.getItem("storage13")
+inputArray[5].value=localStorage.getItem("storage14")
+inputArray[6].value=localStorage.getItem("storage15")
+inputArray[7].value=localStorage.getItem("storage16")
+inputArray[8].value=localStorage.getItem("storage17")
+
+
+function funBtn(){
+let nineText=inputArray[0].value
+let storage0= localStorage.setItem("storage9",(nineText))
+let tenText=inputArray[1].value
+let storage1= localStorage.setItem("storage10",(tenText))
+
+
+}
+
+
 
 
 
